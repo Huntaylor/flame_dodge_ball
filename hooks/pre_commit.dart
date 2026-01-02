9f9a95f88bca9825475b576daf9596a0d96da132
mrgnhnt96 <39742020+mrgnhnt96@users.noreply.github.com>
2026-01-02 11:11:11 -0700

    feat: add more tasks

--- hooks/pre_commit.dart
+++ hooks/pre_commit.dart
@@ -4,17 +4,52 @@ Hook main() {
   return PreCommitHook(
     tasks: [
       ReRegisterHooks(),
-      ShellTask(
-        include: [Glob('**.dart')],
-        commands: (files) {
-          return ['dart format ${files.join(' ')}'];
-        },
-      ),
-      ShellTask(
-        include: [Glob('**.dart')],
-        commands: (files) {
-          return ['dart analyze . --fatal-infos --fatal-warnings'];
-        },
+      SequentialTasks(
+        tasks: [
+          ShellTask(
+            include: [Glob('**.dart')],
+            workingDirectory: 'packages/application',
+            commands: (files) {
+              return ['dart run build_runner build'];
+            },
+          ),
+          ShellTask(
+            include: [Glob('**.dart')],
+            workingDirectory: 'packages/domain',
+            commands: (files) {
+              return ['dart run build_runner build'];
+            },
+          ),
+          ShellTask(
+            include: [Glob('**.dart')],
+            workingDirectory: 'packages/data',
+            commands: (files) {
+              return ['dart run build_runner build'];
+            },
+          ),
+          ParallelTasks(
+            tasks: [
+              ShellTask(
+                include: [Glob('**.dart')],
+                commands: (files) {
+                  return ['barreler build --config barreler.yaml'];
+                },
+              ),
+              ShellTask(
+                include: [Glob('**.dart')],
+                commands: (files) {
+                  return ['dart format ${files.join(' ')}'];
+                },
+              ),
+              ShellTask(
+                include: [Glob('**.dart')],
+                commands: (files) {
+                  return ['dart analyze . --fatal-infos --fatal-warnings'];
+                },
+              ),
+            ],
+          ),
+        ],
       ),
     ],
   );
