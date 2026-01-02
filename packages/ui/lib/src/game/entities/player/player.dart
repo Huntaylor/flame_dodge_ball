16e9c10a3a2aa500de6b378be9b5efdb2868fb6e
Taylor Hunt <62562348+Huntaylor@users.noreply.github.com>
2026-01-02 10:53:00 -0700

    player init, removes linux file

---
+++ packages/ui/lib/src/game/entities/player/player.dart
@@ -0,0 +1,12 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+
+class Player extends SpriteAnimationComponent {
+  Player();
+
+  @override
+  FutureOr<void> onLoad() {
+    return super.onLoad();
+  }
+}
