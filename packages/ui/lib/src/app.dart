f00d5c0f85bf05131fbd7439feeb1ae9c88e2106
mrgnhnt96 <39742020+mrgnhnt96@users.noreply.github.com>
2026-01-02 10:11:38 -0700

    feat: init coordinator

--- packages/ui/lib/src/app.dart
+++ packages/ui/lib/src/app.dart
@@ -2,10 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:ui/src/routes/game_route.dart';
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key, required this.getIt});
 
   final GetIt getIt;
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  void initState() {
+    super.initState();
+    GameCoordinator.init(widget.getIt);
+  }
 
   @override
   Widget build(BuildContext context) {
